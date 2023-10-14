b53c479fd3d032651e222e42c1b98e6290d6c4cb
Amber <woll.ambermadison@gmail.com>
2023-10-13 18:51:20 -0600

    py lab 5

--- python_labs/python_lab_5/main.py
+++ python_labs/python_lab_5/main.py
@@ -2,9 +2,15 @@ import argparse
 
 def main(inputString):
     # Write the code to reverse the string here
+    new_string = ''
+    number_of_characters = len(inputString)
+    for a in range(number_of_characters):
+        new_string += inputString[-1]
+        inputString = inputString[:-1]
+
     # Make sure that your terminal output matches the terminal output of the example given on the instructions.
     
-    return None
+    print(new_string)
 
 if __name__ == "__main__":
     argumentParser = argparse.ArgumentParser("String Reverser")
